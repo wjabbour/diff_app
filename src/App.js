124feeb91878a4f68ba5db58c942e40a06f6e8f8
William <william.jabbour@fedex.com>
2020-01-24 09:22:55 -0600

    Add file reading functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let reader;
+const handleFileChosen = file => {
+  
+  reader = new FileReader();
+  reader.onload = function(event) {
+    var contents = event.target.result;
+    console.log(contents);
+  }
+  reader.readAsText(file);
+}
+
+
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+  }
+
+  componentDidMount () {
+    const script = document.createElement("script");
+
+    script.src = "./diff_match_patch.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+}
+  
+  render() {
+    return (
+      <div>
+        <div>
+          <div className="add-media" >
+              <input
+                name="First File"
+                type="file"
+                id="file"
+                accept=".txt"
+                ref="fileUploader"
+                onChange={ e => handleFileChosen(e.target.files[0])}
+              />
+          </div>
+        </div>
+
+        <div>
+          <div className="add-media" >
+              <input
+              name="First File"
+              type="file"
+              id="file"
+              accept=".txt"
+              ref="fileUploader"
+              onChange={ e => handleFileChosen(e.target.files[0])}
+              />
+          </div>
+        </div>
+
+        <button>Compare</button>
+      </div>
+    )
+  }
 }
 
 export default App;
