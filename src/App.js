27110cc2a97102d7526a214b2c9dd6dac951136c
William <william.jabbour@fedex.com>
2020-02-03 12:38:49 -0600

    Implement file comparison functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
 import React, {Component} from 'react';
 import './App.css';
+var diff = require('diff');
 
 let reader;
-const handleFileChosen = file => {
-  
-  reader = new FileReader();
-  reader.onload = function(event) {
-    var contents = event.target.result;
-    console.log(contents);
-  }
-  reader.readAsText(file);
-}
-
-
+let comparedText;
 
 class App extends Component {
   constructor (props) {
     super(props);
+    this.state = {firstFile: "", secondFile: "", comparisonOutput: ""};
+    this.handleFileA = this.handleFileA.bind(this);
+    this.handleFileB = this.handleFileB.bind(this);
+  }
+
+  handleFileA = file => {
+    let fileContents = "";
+    reader = new FileReader();
+    reader.onload = event => {
+      fileContents = event.target.result;
+      fileContents = fileContents.split('\n');
+      let lineRep = [];
+      fileContents.forEach(line => {
+        lineRep.push(line.split("*"));
+      });
+      let myFile = {
+        name: file.name,
+        lines: lineRep
+      }
+      this.setState({firstFile: myFile});
+    }
+    reader.readAsText(file);
+  }
+  
+  handleFileB = file => {
+    let fileContents = "";
+    reader = new FileReader();
+    reader.onload = event => {
+      fileContents = event.target.result;
+      fileContents = fileContents.split('\n');
+      let lineRep = [];
+      fileContents.forEach(line => {
+        lineRep.push(line.split("*"));
+      });
+      let myFile = {
+        name: file.name,
+        lines: lineRep
+      }
+      this.setState({secondFile: myFile});
+    }
+    reader.readAsText(file);
+  }
+  
+  compareFiles = () => {
+    let comparison = "";
+    let fileA = this.state.firstFile;
+    let fileB = this.state.secondFile;
+    for(let i = 0; i < fileA.lines.length; i++) {
+      for(let j = 1; j < fileA.lines[i].length; j++) {
+
+        if(!(fileA.lines[i][j] === fileB.lines[i][j])) {
+          comparison += `${fileA.name} Line ${i + 1}, Section ${fileA.lines[i][0]}-${j}: ${fileA.lines[i][j]}\r\n${fileB.name} Line ${i + 1}, Section ${fileB.lines[i][0]}-${j}: ${fileB.lines[i][j]}\r\n`
+        }
+      }
+    }
+    if (!(comparison === "")) {
+      this.setState({comparisonOutput: comparison});
+    }
+    
+  }
+
+  downloadTxtFile = () => {
+    const element = document.createElement("a");
+    this.setState({comparisonOutput: "A".bold()});
+    const file = new Blob([this.state.comparisonOutput], {type: 'text/html'});
+    element.href = URL.createObjectURL(file);
+    element.download = "comparison_diff.txt";
+    document.body.appendChild(element); // Required for this to work in FireFox
+    element.click();
   }
   
   render() {
+    let button;
+    const comparisonOutput = this.state.comparisonOutput;
+      if (comparisonOutput) {
+    
+        button = <button onClick={ e => this.downloadTxtFile() }>Download txt</button>
+      
+      }
     return (
       <div>
         <div>
@@ -27,10 +94,10 @@ class App extends Component {
               <input
                 name="First File"
                 type="file"
-                id="file"
+                id="fileA"
                 accept=".txt"
                 ref="fileUploader"
-                onChange={ e => handleFileChosen(e.target.files[0])}
+                onChange={ e => this.handleFileA(e.target.files[0])}
               />
           </div>
         </div>
@@ -40,15 +107,18 @@ class App extends Component {
               <input
               name="First File"
               type="file"
-              id="file"
+              id="fileB"
               accept=".txt"
               ref="fileUploader"
-              onChange={ e => handleFileChosen(e.target.files[0])}
+              onChange={ e => this.handleFileB(e.target.files[0])}
               />
           </div>
         </div>
-
-        <button>Compare</button>
+        
+        <button onClick={ e => this.compareFiles() }>Compare</button>
+        {button}
+        
+        
       </div>
     )
   }
