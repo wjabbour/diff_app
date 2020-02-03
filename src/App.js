5d6859848095acc7cf0fcc4c5c0db2e03bf4d0e3
William <william.jabbour@fedex.com>
2020-02-03 15:15:42 -0600

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 let reader;
 
