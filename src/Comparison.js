790e841ec16c6975fa3ed08a5aa8b3f7372d0f44
William <william.jabbour@fedex.com>
2020-02-03 15:41:04 -0600

    Fix spacing issues in source code

--- src/Comparison.js
+++ src/Comparison.js
@@ -13,67 +13,67 @@ export default class Comparison extends Component {
         let fileContents = "";
         reader = new FileReader();
         reader.onload = event => {
-          fileContents = event.target.result;
-          fileContents = fileContents.split('\n');
-          let lineRep = [];
-          fileContents.forEach(line => {
-            lineRep.push(line.split("*"));
-          });
-          let myFile = {
-            name: file.name,
-            lines: lineRep
-          }
-          this.setState({firstFile: myFile});
+            fileContents = event.target.result;
+            fileContents = fileContents.split('\n');
+            let lineRep = [];
+            fileContents.forEach(line => {
+                lineRep.push(line.split("*"));
+            });
+            let myFile = {
+                name: file.name,
+                lines: lineRep
+            }
+            this.setState({firstFile: myFile});
         }
         reader.readAsText(file);
-      }
+    }
       
-      handleFileB = file => {
+    handleFileB = file => {
         let fileContents = "";
         reader = new FileReader();
         reader.onload = event => {
-          fileContents = event.target.result;
-          fileContents = fileContents.split('\n');
-          let lineRep = [];
-          fileContents.forEach(line => {
-            lineRep.push(line.split("*"));
-          });
-          let myFile = {
-            name: file.name,
-            lines: lineRep
-          }
-          this.setState({secondFile: myFile});
+            fileContents = event.target.result;
+            fileContents = fileContents.split('\n');
+            let lineRep = [];
+            fileContents.forEach(line => {
+                lineRep.push(line.split("*"));
+            });
+            let myFile = {
+                name: file.name,
+                lines: lineRep
+            }
+            this.setState({secondFile: myFile});
         }
         reader.readAsText(file);
-      }
+    }
       
-      compareFiles = () => {
+        compareFiles = () => {
         let configuration = {
-          "GS": [3]
+            "GS": [3]
         }
         let comparison = "";
         let fileA = this.state.firstFile;
         let fileB = this.state.secondFile;
         for(let i = 0; i < fileA.lines.length; i++) {
-          for(let j = 1; j < fileA.lines[i].length; j++) {
-            // if segment name is in conf
-            if (configuration.hasOwnProperty(fileA.lines[i][0])) {
-              if (configuration[fileA.lines[i][0]].includes(j)) {
-                continue;
-              }
+            for(let j = 1; j < fileA.lines[i].length; j++) {
+                // if segment name is in conf
+                if (configuration.hasOwnProperty(fileA.lines[i][0])) {
+                    if (configuration[fileA.lines[i][0]].includes(j)) {
+                        continue;
+                    }
+                }
+                if(!(fileA.lines[i][j] === fileB.lines[i][j])) {
+                    comparison += `${fileA.name} Line ${i + 1}, Section ${fileA.lines[i][0]}-${j}: ${fileA.lines[i][j]}\r\n${fileB.name} Line ${i + 1}, Section ${fileB.lines[i][0]}-${j}: ${fileB.lines[i][j]}\r\n`
+                }
             }
-            if(!(fileA.lines[i][j] === fileB.lines[i][j])) {
-              comparison += `${fileA.name} Line ${i + 1}, Section ${fileA.lines[i][0]}-${j}: ${fileA.lines[i][j]}\r\n${fileB.name} Line ${i + 1}, Section ${fileB.lines[i][0]}-${j}: ${fileB.lines[i][j]}\r\n`
-            }
-          }
         }
         if (!(comparison === "")) {
-          this.setState({comparisonOutput: comparison});
+            this.setState({comparisonOutput: comparison});
         }
         
-      }
+    }
     
-      downloadTxtFile = () => {
+    downloadTxtFile = () => {
         const element = document.createElement("a");
         this.setState({comparisonOutput: "A".bold()});
         const file = new Blob([this.state.comparisonOutput], {type: 'text/html'});
@@ -81,48 +81,44 @@ export default class Comparison extends Component {
         element.download = "comparison_diff.txt";
         document.body.appendChild(element); // Required for this to work in FireFox
         element.click();
-      }
+    }
     render() {
         let button;
         const comparisonOutput = this.state.comparisonOutput;
-            if (comparisonOutput) {
-        
-            button = <button class="btn btn-primary" onClick={ e => this.downloadTxtFile() }>Download txt</button>
-            
-            }
+        if (comparisonOutput) {
+        button = <button class="btn btn-primary" onClick={ e => this.downloadTxtFile() }>Download txt</button>
+        }
         return (
             <div>
-        <div>
-          <div className="add-media" >
-              <input
-                name="First File"
-                type="file"
-                id="fileA"
-                accept=".txt"
-                ref="fileUploader"
-                onChange={ e => this.handleFileA(e.target.files[0])}
-              />
-          </div>
-        </div>
+                <div>
+                    <div className="add-media" >
+                        <input
+                        name="First File"
+                        type="file"
+                        id="fileA"
+                        accept=".txt"
+                        ref="fileUploader"
+                        onChange={ e => this.handleFileA(e.target.files[0])}
+                        />
+                    </div>
+                </div>
 
-        <div>
-          <div className="add-media" >
-              <input
-              name="First File"
-              type="file"
-              id="fileB"
-              accept=".txt"
-              ref="fileUploader"
-              onChange={ e => this.handleFileB(e.target.files[0])}
-              />
-          </div>
-        </div>
-        
-        <button onClick={ e => this.compareFiles() }>Compare</button>
-        {button}
-        
-        
-      </div>
+                <div>
+                    <div className="add-media" >
+                        <input
+                        name="First File"
+                        type="file"
+                        id="fileB"
+                        accept=".txt"
+                        ref="fileUploader"
+                        onChange={ e => this.handleFileB(e.target.files[0])}
+                        />
+                    </div>
+                </div>
+            
+                <button onClick={ e => this.compareFiles() }>Compare</button>
+                {button}
+            </div>
         )
     }
 }
